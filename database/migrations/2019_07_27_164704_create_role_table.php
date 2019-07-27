68211ec1f055ed0aa4864678d4ec866ab374fe38
thedtv-1961 <dam.trieu.van.the@sun-asterisk.com>
2019-07-28 00:06:42 +0700

    Create Models

--- database/migrations/2019_07_27_164704_create_role_table.php
+++ database/migrations/2019_07_27_164704_create_role_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRoleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('role', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('email')->unique();
             $table->string('name');
-            $table->string('password');
-            $table->date('birthday');
-            $table->string('mobile')->nullable();
-            $table->boolean('gender')->default(1);
-            $table->string('avatar')->nullable();
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -34,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('role');
     }
 }
